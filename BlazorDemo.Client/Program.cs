0a8a38e18608282e77cc67503c150ac2e2db3e6e
Gunnar <gpeipman@hotmail.com>
2019-05-18 19:23:09 +0300

    update to .net core 3.0 preview 5

--- BlazorDemo.Client/Program.cs
+++ BlazorDemo.Client/Program.cs
@@ -1,22 +1,9 @@
-﻿using BlazorDemo.Shared;
-using Microsoft.AspNetCore.Blazor.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
 
 namespace BlazorDemo.Client
 {
     public class Program
     {
-        //static void Main(string[] args)
-        //{
-        //    //var serviceProvider = new BrowserServiceProvider(services =>
-        //    //{
-        //    //    services.AddSingleton<IBooksClient, BooksClient>();
-        //    //    //services.AddSingleton<IBooksClient, BooksAzureFunctionsClient>();
-        //    //});
-
-        //    //new BrowserRenderer(serviceProvider).AddComponent<App>("app");
-        //}
-
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
